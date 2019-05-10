702ef241587efd3859c650532d540646f9139f14
Arun Sasi <arunayr@gmail.com>
2019-05-10 15:41:55 +0530

    update

--- assets/js/page.js
+++ assets/js/page.js
@@ -16,18 +16,6 @@ $(function () {
     });
 
 
-    //Hotlist Vehicle
-    $('#hotlist-vehicles-table').dataTable({
-
-        bFilter: false, //hide search box
-        "sDom": "<'row'<'col-sm-12'tr>>" + "<'row'<'col-sm-2'l><'col-sm-3'i><'col-sm-7'p>>", // move filter to bottom
-        'columnDefs': [{
-            'orderable': false,
-            'targets': 0
-        }], // hide sort icon on header of first column
-        'aaSorting': [[1, 'asc']] // start to sort data in second column 
-    });
-
 
 });
 
